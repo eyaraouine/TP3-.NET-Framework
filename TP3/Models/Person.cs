fe8133c5c7abbe3600b8b2d5798065877851723e
eyaraouine <eyaraouine15@gmail.com>
2022-12-04 08:30:27 +0100

    first commit

---
+++ TP3/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+
+namespace TP3.Models
+{
+    public class Person
+    {
+     
+        public Person(int id, string firstName, string lastName, string email, string image, string country)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Image = image;
+            Country = country;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string Image { get; set; }
+
+    }
+}
